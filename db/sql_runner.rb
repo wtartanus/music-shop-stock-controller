d3febabccbc6c209a0e6ee4d7f1d7812a0c56cb6
Wojtek Tartanus <wtartanus@gmail.com>
2016-05-20 14:28:15 +0100

    added sql runner

---
+++ db/sql_runner.rb
@@ -0,0 +1,12 @@
+class SqlRuner
+
+  def self.run( sql )
+    begin
+      db = PG.connect( { dbname: 'music_library', host: 'localhost'} )
+      db.exex( sql )
+    ensure
+      db.close()
+    end
+  end
+
+end
